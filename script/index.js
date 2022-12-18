7602ccfcc600d516db655908254e93bdaed1c47f
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2022-12-18 13:41:27 +0330

    add loadParagraph func

--- script/index.js
+++ script/index.js
@@ -1,17 +1,26 @@
-import {paragraphs} from './paragraphs.js';
+import { paragraphs } from "./paragraphs.js";
 
-const typingText = document.querySelector('.typing-text p')
+const typingText = document.querySelector(".typing-text p");
 
-const inpField = document.querySelector('.wrapper .input-field')
-const tryAgainBtn = document.querySelector(".content button")
-  const timeTag = document.querySelector(".time span b")
-  const mistakeTag = document.querySelector(".mistake span")
-  const wpmTag = document.querySelector(".wpm span")
-  const cpmTag = document.querySelector(".cpm span");
+const inpField = document.querySelector(".wrapper .input-field");
+const tryAgainBtn = document.querySelector(".content button");
+const timeTag = document.querySelector(".time span b");
+const mistakeTag = document.querySelector(".mistake span");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
 
-  let timer;
-  let maxTime = 60;
-  let timeLeft =maxTime;
-  let charIndex = mistakes = isTyping = 0;
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = (mistakes = isTyping = 0);
 
-  
+function loadParagraph() {
+  const ranIndex = Math.floor(Math.random() * paragraphs.length);
+  paragraphs[ranIndex].split("").forEach((char) => {
+    let span = `<span>${char}</span>`;
+    typingText.innerHTML += span;
+  });
+  typingText.querySelectorAll("span")[0].classList.add("active");
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
+}
