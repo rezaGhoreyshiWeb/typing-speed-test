eb97735493f8424026e7273ccffee73530737366
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2022-12-18 13:33:40 +0330

    query the elements

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
-import {paragraphs} from './paragraphs.js'
+import {paragraphs} from './paragraphs.js';
+
+const typingText = document.querySelector('.typing-text p')
+
+const inpField = document.querySelector('.wrapper .input-field')
+const tryAgainBtn = document.querySelector(".content button")
+  const timeTag = document.querySelector(".time span b")
+  const mistakeTag = document.querySelector(".mistake span")
+  const wpmTag = document.querySelector(".wpm span")
+  const cpmTag = document.querySelector(".cpm span");
+
+  
